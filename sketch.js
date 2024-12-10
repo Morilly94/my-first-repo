1077f4533a0ad3be1d51229f46e0eca745b34d65
Morilly94 <m.jones0620211@arts.ac.uk>
2024-12-10 15:12:44 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(650, 750);
 }
 
 function draw() {
   background('red');
-  circle(200, 50, 50);
-
+  circle(200, 250, 250);
+  circle(100, 100, 100);
+  circle(300, 450, 200);
+  circle(250, 90, 150);
+  circle(500, 75, 300);
+  circle(300, 470, 500);
+  circle(200, 200, 180);
+  
+function drawGiraffe(size) {
+    // Style the text.
+    textAlign(CENTER, CENTER);
+    textSize(size);
+  
+    text('🦒', 50, 50);
 }
